85be19892700e7e7a87c0665c84e88b25f03bfc7
jmorton <mortson@hotmail.co.uk>
2023-01-22 20:24:09 +0000

    UseRef

--- src/webcamTwo.tsx
+++ src/webcamTwo.tsx
@@ -1,62 +1,65 @@
-import React, {useState, useRef} from 'react';
+import React, {useEffect, useRef, MutableRefObject} from 'react';
 import { MediaTrackConstraintsOES } from './interfaces/MediaStreamTrackSetOES';
 
-function Two(props:any) {
+function Two() {
 
-    const outputText = document.getElementById("outputText")!;
-    const outputConstraintError = document.getElementById("outputConstraintError")!;
-    const outputFlipState = document.getElementById("outputFlipState");
+	const outputText = document.getElementById("outputText")!;
+	const outputConstraintError = document.getElementById("outputConstraintError")!;
 
-    let streamRef: MediaStream;
+	const streamRef: MutableRefObject<MediaStream | null> = useRef(null);
 
-    const constraints = {
-        video: {
-            facingMode: { ideal: 'environment' },
-        }
-    }
+	const torchOne = {torch: true};
+	const torchTwo = {advanced: [{torch: true}]};
 
-    const torchOne = {torch: true};
-    const torchTwo = {advanced: [{torch: true}]};
+	useEffect(() => {
+		const constraints = {
+			video: {
+				facingMode: { ideal: "environment" },
+			},
+		};
+	
+		navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+			streamRef.current = stream;
+			console.log(stream);
+		});
+	}, [streamRef]);
 
-    navigator.mediaDevices.getUserMedia(constraints)
-    .then(stream => { 
-        streamRef = stream
-        console.log(stream);
-    })
+	function applyTorch(
+		stream: MutableRefObject<MediaStream | null>,
+		torchObj: MediaTrackConstraintsOES,
+		trigger: string
+	) {
+		let track = stream.current?.getVideoTracks()[0];
+	
+		console.log(track);
+		console.log(torchObj);
+	
+		track?.applyConstraints(torchObj).catch((err) => {
+			if (err instanceof OverconstrainedError) {
+				outputConstraintError.textContent = err.constraint;
+			}
+			outputText.textContent = `${trigger} Failed`;
+		});
+	}
 
-    function applyTorch(stream:MediaStream, torchObj: MediaTrackConstraintsOES, trigger: string) {
-        let track = stream.getVideoTracks()[0];
-        console.log(track);
-        console.log(torchObj)
-
-        track.applyConstraints(torchObj)
-            .catch(err => {
-                if (err instanceof OverconstrainedError){
-                    outputConstraintError.textContent = err.constraint;
-                } 
-                outputText.textContent = `${trigger} Failed`;
-            });
-            
-    }
-
-    function logConstraints(stream: MediaStream) {
-        let track = stream.getVideoTracks()[0];
-        console.log(`Settings:`, track.getSettings());
-        console.log(`Constraints:`, track.getConstraints());
-        console.log(`Capabilities:`, track.getCapabilities());
-    }
+	function logConstraints(stream: MutableRefObject<MediaStream | null>) {
+		let track = stream.current?.getVideoTracks()[0];
+		console.log(`Settings:`, track?.getSettings());
+		console.log(`Constraints:`, track?.getConstraints());
+		console.log(`Capabilities:`, track?.getCapabilities());
+	}
 
    
 
-    return (
-        <div>
-            <div>
-                <button onClick={() => { (streamRef) ? applyTorch(streamRef, torchOne, "Torch V1") : console.log("Stream not found")}}>Torch V1</button>
-                <button onClick={() => { (streamRef) ? applyTorch(streamRef, torchTwo, "Torch V2") : console.log("Stream not found")}}>Torch V2</button>
-            </div>
-            <button onClick={() => { (streamRef) ? logConstraints(streamRef) : console.log("Stream not found")}}>Log Constraints</button>
-        </div>
-    )
+	return (
+		<div>
+			<div>
+				<button onClick={() => { (streamRef) ? applyTorch(streamRef, torchOne, "Torch V1") : console.log("Stream not found")}}>Torch V1</button>
+				<button onClick={() => { (streamRef) ? applyTorch(streamRef, torchTwo, "Torch V2") : console.log("Stream not found")}}>Torch V2</button>
+			</div>
+			<button onClick={() => { (streamRef) ? logConstraints(streamRef) : console.log("Stream not found")}}>Log Constraints</button>
+		</div>
+	)
 }
 
 export default Two;
